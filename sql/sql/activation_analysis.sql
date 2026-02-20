6d33c8a0171e99eca94596633e7b74581db72999
Coumba-guaye <117094127+Coumba-guaye@users.noreply.github.com>
2026-02-20 03:11:32 +0100

    Create activation_analysis.sql

---
+++ sql/sql/activation_analysis.sql
@@ -0,0 +1,8 @@
+
+---
+
+## 📌 Conclusion
+
+L’amélioration du taux d’activation représente le levier stratégique prioritaire pour augmenter la conversion et le MRR.
+
+Projet réalisé dans une logique d’analyse produit orientée décision.
